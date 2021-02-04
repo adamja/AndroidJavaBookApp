ff36a896644ab8c5df83ab5c68e00197d63ebf95
AJ <adam_289@hotmail.com>
2021-02-04 12:28:26 +0800

    Created the main screen and added a new activity

--- app/src/main/java/com/example/bookapp/MainActivity.java
+++ app/src/main/java/com/example/bookapp/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.bookapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAllBooks, btnAlreadyRead, btnWishlist, btnCurrentlyReading, btnFavourite, btnAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        btnAllBooks = findViewById(R.id.btnAllBooks);
+        btnAlreadyRead = findViewById(R.id.btnAlreadyRead);
+        btnWishlist = findViewById(R.id.btnWishlist);
+        btnCurrentlyReading = findViewById(R.id.btnCurrentlyReading);
+        btnFavourite = findViewById(R.id.btnFavourite);
+        btnAbout = findViewById(R.id.btnAbout);
     }
 }
